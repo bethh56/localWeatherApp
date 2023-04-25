52f33d4b3dc09637708ec9ad8449bb374ffaf884
Beth Nielsen <bethhnielsen56@gmail.com>
2023-04-25 15:20:53 -0400

    first commit

---
+++ src/App/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TEST</h1>
+      <button type="button" class="btn btn-warning">Warning</button>
+      <i class="fa-solid fa-magnifying-glass"></i>
+    </div>
+  );
+}
+
+export default App;
